4ec73399984398a2af9191bf2e49eef4e539b05a
ishankoske-svg <ishankoske-svg@users.noreply.github.com>
2026-06-08 11:10:13 +0530

    Add Navbar to App layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-slate-50 text-slate-900 font-sans selection:bg-indigo-100 selection:text-indigo-900">
+        <Navbar />
         <Routes>
           <Route path="/" element={<DashboardPage />} />
         </Routes>
